0dc306ca2bb9d616d0ed2bea2539e6f3814c12b6
erica-monacillo <ericamonacillo09@gmail.com>
2025-11-21 16:57:01 +0800

    go

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,19 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-using namespace std;
-
 int main() {
-    // VideoMode now expects sf::Vector2u
     sf::RenderWindow window(sf::VideoMode({800, 600}), "DFA SQL Injection Visualizer");
 
     while (window.isOpen()) {
-        // pollEvent now returns std::optional<Event>
         while (auto eventOpt = window.pollEvent()) {
-            auto& event = *eventOpt; // get the actual event
-
+            auto& event = *eventOpt;
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear(sf::Color::White);
 
-        // Draw DFA states here later
+        // Draw your DFA states here
 
         window.display();
     }
